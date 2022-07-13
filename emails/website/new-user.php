18d61c85c347fc064bc4c0003236787f5c58e481
Jonathan Stegall <jstegall@minnpost.com>
2022-07-13 10:27:58 -0500

    the new user email template

---
+++ emails/website/new-user.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<!--[if !mso]><!-->
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<!--<![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title><?php echo 'Welcome to MinnPost'; ?></title>
+		<style type="text/css" data-premailer="ignore">
+			.webkit {
+				max-width: 600px;
+			}
+			.ExternalClass {
+				width: 100%;
+			}
+			.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+				line-height: 100%;
+			}
+
+			a[x-apple-data-detectors] {
+				color: #801019 !important;
+				text-decoration: none !important;
+				font-size: inherit !important;
+				font-family: inherit !important;
+				font-weight: normal !important;
+				line-height: inherit !important;
+			}
+		</style>
+
+		<!--[if (gte mso 9)|(IE)]>
+		<style type="text/css" data-premailer="ignore">
+		.header table { font-size:1px; line-height:0; mso-margin-top-alt:1px; }
+		</style>
+		<![endif]-->
+
+	<style>body {
+margin: 0; padding: 0; min-width: 100%; background-color: #ffffff;
+}
+img {
+border: 0 none; height: auto; line-height: 100%; outline: none; text-decoration: none;
+}
+a:hover {
+color: #801019 !important; text-decoration: underline !important;
+}
+a:active {
+color: #801019 !important; text-decoration: underline !important;
+}
+a:visited {
+color: #801019 !important;
+}
+h3 a:active {
+color: #1A1818;
+}
+h5 a:active {
+color: #1A1818;
+}
+h3 a:visited {
+color: #1A1818;
+}
+h5 a:visited {
+color: #1A1818;
+}
+.content .button.read-story td td a:hover {
+color: #ffffff !important; text-decoration: none !important;
+}
+.content .button.read-story td td a:visited {
+color: #ffffff !important; text-decoration: none !important;
+}
+.content .button.read-story td td a:active {
+color: #ffffff !important; text-decoration: none !important;
+}
+@media only screen and (max-width: 600px) {
+	.one-column .column {
+		max-width: 90% !important; margin-right: auto !important; margin-left: auto !important;
+	}
+}
+</style></head>
+	<body style="min-width: 100%; margin: 0; padding: 0;" bgcolor="#ffffff">
+
+	<!-- limit to 50 characters; only display if there is a value -->
+	<span style="display: none !important; font-size: 0; color: #fff;"> <!-- gmail only takes display none if it has !important; outlook still doesn't care -->
+		<?php echo 'Welcome to your MinnPost.com website account.'; ?>
+	</span>
+
+	<center class="wrapper" style="width: 100%; table-layout: fixed; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;">
+		<div class="webkit">
+
+			<!--[if (gte mso 9)|(IE)]>
+				<table cellpadding="0" cellspacing="0" width="600" align="center">
+					<tr>
+						<td>
+						<![endif]-->
+				<table cellpadding="0" cellspacing="0" class="outer" align="center" style="border-spacing: 0; margin: 0 auto; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; width: 100%; max-width: 600px;">
+
+					<tr>
+						<td class="one-column header" style="border-collapse: collapse; font-size: 0; border-bottom-width: 10px; border-bottom-color: #000; border-bottom-style: solid; margin: 0; padding: 0;" align="center">
+						<!--[if (gte mso 9)|(IE)]>
+						<table cellpadding="0" cellspacing="0" width="100%">
+							<tr>
+								<td width="100%" valign="bottom">
+						<![endif]-->
+							<div class="column logo" style="vertical-align: bottom; margin: 0;">
+								<table cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+									<tr>
+										<td class="inner" style="border-collapse: collapse; margin: 0; padding: 0; max-height: 90px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+											<table cellpadding="0" cellspacing="0" class="contents" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; width: 100%; font-size: 16px; text-align: left;">
+												<tr>
+													<td style="border-collapse: collapse; margin: 0; padding: 0; max-height: 90px; mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-size: 0; line-height: 0px;" valign="bottom">
+														<a href="https://www.minnpost.com/" style="color: #801019; text-decoration: none;"><img src="https://www.minnpost.com/wp-content/uploads/sites/default/themes/siteskin/inc/images/newsletter-logo-daily.png" alt="<?php echo 'MinnPost'; ?>" width="390" height="90" align="left" style="height: auto; margin: 0; padding: 0; border: 0 none; line-height: 100%; outline: none; text-decoration: none; width: 100%; max-width: 390px; max-height: 90px; display: block; vertical-align: bottom;" /></a>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+							</div>
+						<!--[if (gte mso 9)|(IE)]>
+								</td>
+							</tr>
+						</table>
+						<![endif]-->
+						</td> <!-- end .two-column.header -->
+					</tr> <!-- end row -->
+
+					<tr>
+						<td class="one-column content story" style="border-collapse: collapse; font-size: 0; margin: 0; padding: 0;" align="center">
+						<!--[if (gte mso 9)|(IE)]>
+						<table cellpadding="0" cellspacing="0" width="100%">
+							<tr>
+								<td width="100%" valign="middle">
+						<![endif]-->
+							<div class="column story story-first">
+									<div class="story-inner" style="border-bottom-width: 2px; border-bottom-color: #cccccf; border-bottom-style: solid; padding-bottom: 18px; margin-bottom: 18px; margin-top: 18px;">
+										<table cellpadding="0" cellspacing="0" class="bodyTable" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; text-align: left;">
+											<tr>
+												<td valign="top" style="border-collapse: collapse; margin: 0; padding: 0;">
+													<div>
+														<p style="font-family: Georgia, 'Times New Roman', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 10px; padding: 0;">Hi <?php echo 'Jonathan Stegall' ?>,</p>
+														<p style="font-family: Georgia, 'Times New Roman', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 10px; padding: 0;">Thanks for creating your MinnPost.com user account. You can log in with your email address, <a href="mailto:<?php echo 'jstegall@minnpost.com'; ?>" style="color: #801019; text-decoration: none;"><?php echo 'jstegall@minnpost.com'; ?></a>. Use your account to manage your reading and communication preferences, and to see new stories that match your interests.</p>
+
+														<p style="font-family: Georgia, 'Times New Roman', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 10px; padding: 0;">If you have not set up your preferences yet, you can <a href="<?php echo 'https://www.minnpost.com/user/preferences/'; ?>" style="color: #801019; text-decoration: none;">do that now</a>.</p>
+
+														<p style="font-family: Georgia, 'Times New Roman', Times, serif; font-size: 16px; line-height: 20.787px; margin: 0 0 10px; padding: 0;">If you are a MinnPost Member, you'll be able to use some of your member benefits from your account. If not, why not <a href="https://support.minnpost.com/" style="color: #801019; text-decoration: none;">join today</a>?</p>
+
+														<table cellpadding="0" cellspacing="0" class="button read-story" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+															<tr>
+																<td style="border-collapse: collapse; margin: 0; padding: 0;">
+																	<table cellpadding="0" cellspacing="0" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+																		<tr>
+																			<td align="center" style="border-collapse: collapse; -webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px; margin: 0; padding: 0;" bgcolor="#801019"><a href="<?php echo 'https://www.minnpost.com/user/'; ?>" target="_blank" style="color: #ffffff; padding: 8px 12px; border: 1px solid #801019; text-decoration: none; font-family: Georgia, 'Times New Roman', Times, serif; font-size: 16px; -webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px; display: inline-block;"><!--[if mso]>&nbsp;<![endif]-->Access your MinnPost account<!--[if mso]>&nbsp;<![endif]--></a></td>
+																		</tr>
+																	</table>
+																</td>
+															</tr>
+														</table>
+													</div>
+												</td>
+											</tr>
+										</table>
+									</div> <!-- end .story-inner -->
+								</div> <!-- end .story -->
+								<!--[if (gte mso 9)|(IE)]>
+									</td>
+								</tr>
+							</table>
+							<![endif]-->
+							</td> <!-- end .two-column.content.story -->
+						</tr> <!-- end row -->
+
+				<tr>
+						<td class="one-column footer" style="border-collapse: collapse; font-size: 0; margin: 0; padding: 0;" align="center">
+							<table cellpadding="0" cellspacing="0" width="100%" style="border-spacing: 0; margin: 0; padding: 0; font-family: Helvetica, Arial, Geneva, sans-serif; color: #1a1818; mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse;">
+								<tr>
+									<td class="inner contents" style="border-collapse: collapse; width: 100%; font-size: 16px; margin: 0; padding: 0;" align="left">
+									<div class="footer-hat" style="Margin-top: 20px; padding: 10px; border: 1px solid #cccccf; background: #f0f0f5; font-family: Helvetica, Arial, Geneva, sans-serif;">
+										<p style="Margin: 0; padding: 0; font-weight: bold; line-height: 1.3em; color: #1a1818;font-family: Helvetica, Arial, Geneva, sans-serif;">
+										MinnPost is a nonprofit newsroom that digs deep into the news that matters. Here is how you can support us:
+										</p>
+										<table style="border-collapse: collapse; border-spacing: 0; color: #1a1818; Margin: 0; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding: 0;vertical-align: top;" width="100%" cellspacing="0" cellpadding="0">
+											<tbody>
+												<tr>
+													<td style="border-collapse: collapse; Margin: 0; padding: 0;vertical-align: top;">
+														<ul style="Margin: 0; padding: 15px 0 0 30px; list-style: disc outside;">
+															<li style="Margin: 0; padding: 0 0 8px 0;"><a href="https://www.minnpost.com/newsletters/" style="color: #801018;text-decoration: none;font-weight: bold;">Subscribe</a> to one of our other free newsletters</li>
+															<li style="Margin: 0; padding: 0 0 8px 0;"><a href="https://www.minnpost.com/support/" style="color: #801018;text-decoration: none;font-weight: bold;">Become</a> a MinnPost member</li>
+														</ul>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+									<div class="footer-bottom" style="padding: 10px 0; text-align: center;">
+										<p style="Margin: 0; padding: 0;">
+											<a href="https://twitter.com/MinnPost" class="twitter" style="color: #801019; font-family: Helvetica, Arial, Geneva, sans-serif; font-size: 13px; font-weight: bold; line-height: 100%; text-align: center; text-decoration: none;">
+												<img class="no-lazy" src="https://www.minnpost.com/wp-content/uploads/2019/03/twitter.png?w=30" alt="Follow us on Twitter" width="25" height="25" align="none">
+											</a><!--[if mso]>&nbsp;&nbsp;<![endif]-->
+											<a href="https://www.facebook.com/MinnPost" class="facebook" style="color: #801019; font-family: Helvetica, Arial, Geneva, sans-serif; font-size: 13px; font-weight: bold; line-height: 100%; text-align: center; text-decoration: none;"><img class="no-lazy" src="https://www.minnpost.com/wp-content/uploads/2019/03/facebook.png?w=30" alt="Like us on Facebook" width="25" height="25" align="none"></a>
+											<!--[if mso]>&nbsp;&nbsp;<![endif]-->
+											<a href="https://www.instagram.com/MinnPost" class="instagram" style="color: #801019; font-family: Helvetica, Arial, Geneva, sans-serif; font-size: 13px; font-weight: bold; line-height: 100%; text-align: center; text-decoration: none;"><img class="no-lazy" src="https://www.minnpost.com/wp-content/uploads/2019/06/instagram.png?w=30" alt="Follow us on Instagram" width="25" height="25" align="none"></a>
+											<a href="mailto:feedback@minnpost.com" class="contact" style="color: #801019; font-family: Helvetica, Arial, Geneva, sans-serif; font-size: 13px; font-weight: bold; line-height: 100%; text-align: center; text-decoration: none;">Contact Us</a>
+										</p>
+									</div>
+										<?php
+										$footer_message = 'MinnPost | P.O. Box 18438 | Minneapolis, MN 55418 | <a href="tel:612.455.6950">612.455.6950</a>';
+										if ( '' !== $footer_message ) :
+											?>
+											<p class="address" style="font-size: 12px; Margin: 10px 0 15px; padding: 0; text-align: center;" align="center"><?php echo $footer_message; ?></p>
+										<?php endif; ?>
+									</td>
+								</tr>
+							</table>
+						</td> <!-- end .one-column.footer -->
+					</tr> <!-- end row -->
+
+				</table> <!-- end .outer -->
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+					</tr>
+				</table>
+			<![endif]-->
+
+		</div>
+	</center>
+
+	</body>
+</html>
